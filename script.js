1f8ce560965772274e88fc7d54e40fc035de3bab
scarty1818 <156967352+scarty1818@users.noreply.github.com>
2024-02-05 21:11:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ require([
   "esri/widgets/Home",
   "dojo/domReady!"
 ], function(WebScene, SceneView, Camera, Home) {
-  
+
   var scene = new WebScene({
     portalItem: {
       id: "fbbbaa2fbfda41b8b3f96427c3ac5c79"
     }
   });
-  
+
   var camera = new Camera({
-    position: [-71.06613, 42.35511, 2500], // elevation in meters
+    position: [-71.06613, 42.35511, 2500], // Elevation in meters
     tilt: 0,
     heading: 0
   });
-  
+
   var camera2 = new Camera({
     position: {
       x: -71.09893,
@@ -27,7 +27,7 @@ require([
     tilt: 0,
     heading: 0
   });
-  
+
   var camera3 = new Camera({
     position: {
       x: -70.974581,
@@ -37,7 +37,7 @@ require([
     tilt: 30, // Tilted downwards for a good view of downtown
     heading: 270 // Facing west towards downtown Boston
   });
-  
+
   var view = new SceneView({
     container: "viewDiv",
     map: scene,
@@ -51,33 +51,33 @@ require([
       }
     }
   });
-  
+
   var homeBtn = new Home({
     view: view
   });
 
   view.ui.add(homeBtn, "top-left");
-  
-  // Ensure buttons are defined in your HTML with these IDs
-  var bc = document.getElementById('bc'); // Button for camera
-  var gm = document.getElementById('gm'); // Button for camera2
-  var seaview = document.getElementById('seaview'); // Button for camera3
-  
-  if(bc && gm && seaview) {
-    bc.addEventListener('click', function() {
-      view.goTo(camera);
-    });
-    
-    gm.addEventListener('click', function() {
-      view.goTo(camera2);
-    });
-    
-    seaview.addEventListener('click', function() {
-      view.goTo(camera3);
-    });
-  } else {
-    console.error("One or more buttons are not found in the HTML document.");
-  }
 
+  // Wait for the DOM to fully load
+  document.addEventListener('DOMContentLoaded', function() {
+    var bc = document.getElementById('bc'); // Button for Boston Commons view
+    var gm = document.getElementById('gm'); // Button for Gardner Museum view
+    var seaview = document.getElementById('seaview'); // Button for Boston Harbour view
+
+    if(bc && gm && seaview) {
+      bc.addEventListener('click', function() {
+        view.goTo(camera);
+      });
+
+      gm.addEventListener('click', function() {
+        view.goTo(camera2);
+      });
+
+      seaview.addEventListener('click', function() {
+        view.goTo(camera3);
+      });
+    } else {
+      console.error("One or more buttons are not found in the HTML document.");
+    }
+  });
 });
-
